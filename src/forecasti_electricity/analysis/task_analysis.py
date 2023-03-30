2fcd243e6c157bd0afae710623cd5317977735ce
omererhanerbis <s6omerbi@uni-bonn.de>
2023-03-30 23:09:34 +0300

    task_analysis prepared without example dependency which are removed into comments for reference writing to be deleted later

--- src/forecasti_electricity/analysis/task_analysis.py
+++ src/forecasti_electricity/analysis/task_analysis.py
@@ -1,11 +1,1 @@
 """Tasks running the core analyses."""
-
-import pandas as pd
-import pytask
-
-from forecasti_electricity.analysis.model import fit_logit_model, load_model
-from forecasti_electricity.analysis.predict import predict_prob_by_age
-from forecasti_electricity.config import BLD, GROUPS, SRC
-from forecasti_electricity.utilities import read_yaml
-
-
