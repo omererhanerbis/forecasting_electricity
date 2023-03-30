610f5d7f95eea674a741d01501a8c34e8e15ce66
omererhanerbis <s6omerbi@uni-bonn.de>
2023-03-30 21:01:58 +0300

    __init__.py adjusted to accommodate name change of clean_data module to restructure_data, missed data_info.yaml adjustment message from previous commit is provided

--- src/forecasti_electricity/data_management/__init__.py
+++ src/forecasti_electricity/data_management/__init__.py
@@ -1,5 +1,5 @@
 """Functions for managing data."""
 
-from forecasti_electricity.data_management.clean_data import clean_data
+from forecasti_electricity.data_management.restructure_data import restructure_data
 
-__all__ = [clean_data]
+__all__ = [restructure_data]
