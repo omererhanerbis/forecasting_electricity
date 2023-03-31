60e093bbbc487bdab01a98dfe80d13851d5333d9
omererhanerbis <s6omerbi@uni-bonn.de>
2023-03-31 05:35:28 +0300

    final init adjusted, plot.py and task_final.py provided with initial output codes

--- src/forecasti_electricity/final/__init__.py
+++ src/forecasti_electricity/final/__init__.py
@@ -1,5 +1,5 @@
 """Functions for formatting results."""
 
-from forecasti_electricity.final.plot import plot_regression_by_age
+from forecasti_electricity.final.plot import plot_consumption
 
-__all__ = [plot_regression_by_age]
+__all__ = [plot_consumption]
--- src/forecasti_electricity/final/task_final.py
+++ src/forecasti_electricity/final/task_final.py
@@ -3,7 +3,36 @@
 import pandas as pd
 import pytask
 
-from forecasti_electricity.analysis.model import load_model
-from forecasti_electricity.config import BLD, GROUPS, SRC
-from forecasti_electricity.final import plot_regression_by_age
+from forecasti_electricity.config import BLD, SRC
+from forecasti_electricity.final import plot_consumption
 from forecasti_electricity.utilities import read_yaml
+
+
+@pytask.mark.depends_on(
+    {
+        "data": BLD / "python" / "data" / "hourly_data_clean.csv",
+        "data_info": SRC / "data_management" / "data_info.yaml",
+    },
+)
+@pytask.mark.produces(BLD / "python" / "figures" / "hourly_consumption.png")
+def task_plot_hourly_consumption(depends_on, produces):
+    """Plot the consumption data."""
+    data_info = read_yaml(depends_on["data_info"])
+    data = pd.read_csv(depends_on["data"])
+    fig = plot_consumption(data, data_info)
+    fig.savefig(produces)
+
+
+@pytask.mark.depends_on(
+    {
+        "data": BLD / "python" / "data" / "daily_data_clean.csv",
+        "data_info": SRC / "data_management" / "data_info.yaml",
+    },
+)
+@pytask.mark.produces(BLD / "python" / "figures" / "daily_consumption.png")
+def task_plot_daily_consumption(depends_on, produces):
+    """Plot the consumption data."""
+    data_info = read_yaml(depends_on["data_info"])
+    data = pd.read_csv(depends_on["data"])
+    fig = plot_consumption(data, data_info)
+    fig.savefig(produces)
